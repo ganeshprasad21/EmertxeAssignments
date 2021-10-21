fcd4ec97de7e4786d55946f8e2d4dbbe36bb6fb7
TheBluePhilosopher <ganesh21111999@gmail.com>
2021-10-22 00:26:57 +0530

    '

---
+++ assignments/LinuxSys/06_assignment.sh
@@ -0,0 +1,44 @@
+<<comments
+name: Ganesh Prasad R
+
+date: 18/10/2021
+
+description: 
+Write a script to print a given number in reverse order
+
+input : /06_reverse.sh 1540
+
+output: 
+Reversed number – 451
+
+comments
+
+#!/bin/bash
+function reverseNumber()
+{
+    ((num=`echo " $1 + 0" | bc`)) #protect from trailing 0s as it used to convert str to weird numbers
+    reversedNumber=0
+    while [ $num -gt 0 ]
+    do
+        ((reminder=`echo $num % 10 | bc`))
+        ((num=$num / 10 ))
+        ((reversedNumber=(($reversedNumber * 10)) + reminder))
+    done
+    echo $reversedNumber
+}
+
+if [ $# -eq 0 ]
+then
+    echo "Error: No argument passed"
+elif [[ $1 =~ ^[0-9]+$ ]]
+then
+    if [ $1 -gt 9 ]
+    then
+        echo -n "Reversed number – "
+        echo `reverseNumber $1`
+    else
+        echo "Error: pass a multi-digit number"
+    fi
+else
+    echo "Enter only numbers"
+fi
