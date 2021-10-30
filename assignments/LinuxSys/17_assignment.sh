77144255d4dfe7539343291655cdeda393c7a4d9
TheBluePhilosopher <ganesh21111999@gmail.com>
2021-10-30 21:42:25 +0530

    new

--- assignments/LinuxSys/17_assignment.sh
+++ assignments/LinuxSys/17_assignment.sh
@@ -1,42 +1,45 @@
 <<comments
 name: Ganesh Prasad R
 
-date: 18/10/2021
+date: 30/10/2021
 
 description: 
-Read 'n' and generate a pattern given below
-1
-1 2
-1 2 3
-1 2 3 4
+Write script to print contents of file from given line number to next given
+number of lines.
 
-input : bash 01_assignment.sh
-Enter the number : 4
+input : ./17_print_lines.sh 5 3 myfile.txt
 
 output: 
-1
-1 2
-1 2 3
-1 2 3 4
+line number 5
+line number 6
+line number 7
 
 comments
 
 #!/bin/bash
 
-read -p "Enter the number :" num
-
-if [ $num -ge 2 -a $num -le `echo "2 * 2 * 2 * 2 * 2" | bc` ]
+if [ $# -eq 3 ] #valid case
 then
-    for row in $(seq 1 $num)
-    do
-        displayNumber=1
-        for col in $(seq 1 $row)        
-        do
-            echo -n "$displayNumber "
-            ((displayNumber=displayNumber + 1))
-        done
-        echo
-    done
-else
-    echo "Error : Number out of range, Please enter 2 < number < 2^5"
+    if [ -e $3 ] #check if file exists
+    then
+        if [ -s $3 ] #check if its not empty
+        then
+            totalLineInAFile=`wc -l $3 | cut -d ' ' -f1` #to get how many lines the given file hasd
+            totalLineForHead=`echo "$1 + $2 - 1" | bc` #to get count for head command
+            if [ $totalLineInAFile -gt $totalLineForHead ]
+            then
+                head -$totalLineForHead $3 | tail -$2 #display required lines
+            else
+                echo "Error: $3 is having only $totalLineInAFile lines, file should have atleast $totalLineForHead lines. " 
+            fi
+        else
+            echo "Empty file passed"
+        fi
+    else
+        echo "File does not exist"
+    fi
+else #the cla is not correct
+    echo "Error: arguments missing!"
+    echo "Usage: ./file_filter.sh start_line uptoline filename"
+    echo "For eg. ./20_file_filter.sh 5 5 <file>"
 fi
