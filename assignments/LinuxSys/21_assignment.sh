7a0cb6469ad1ce723db99bc63d68b35ed647fb80
theBluePhilosopher <ganesh21111999@gmail.com>
2021-11-09 03:15:47 +0530

    done

--- assignments/LinuxSys/21_assignment.sh
+++ assignments/LinuxSys/21_assignment.sh
@@ -1,42 +1,48 @@
 <<comments
 name: Ganesh Prasad R
 
-date: 18/10/2021
+date: 9/11/2021
 
 description: 
-Read 'n' and generate a pattern given below
-1
-1 2
-1 2 3
-1 2 3 4
 
-input : bash 01_assignment.sh
-Enter the number : 4
+
+input : 
 
 output: 
-1
-1 2
-1 2 3
-1 2 3 4
+Write script called say_hello, which will print greetings based on time.
 
 comments
 
 #!/bin/bash
 
-read -p "Enter the number :" num
+timeIn24H=`date +%H` #get required data
+dayNo=`date +%d`
+day=`date +%A`
+month=`date +%B`
+year=`date +%Y`
+timeIn12=`date | tr -s " " | cut -d " " -f4-5`
 
-if [ $num -ge 2 -a $num -le `echo "2 * 2 * 2 * 2 * 2" | bc` ]
+if [ `echo "$timeIn24H >= 5 " | bc ` -eq 1 -a `echo "$timeIn24H < 12 " | bc ` -eq 1 ] #check time and print accordingly
 then
-    for row in $(seq 1 $num)
-    do
-        displayNumber=1
-        for col in $(seq 1 $row)        
-        do
-            echo -n "$displayNumber "
-            ((displayNumber=displayNumber + 1))
-        done
-        echo
-    done
-else
-    echo "Error : Number out of range, Please enter 2 < number < 2^5"
+    echo "Good morning user, Have nice day!"
+    echo "This is $day  $dayNo in $month of $year ($timeIn12)" #display
+elif [ `echo "$timeIn24H >= 12 " | bc ` -eq 1 -a `echo " $timeIn24H <= 13 " | bc ` -eq  1 ]
+then
+    echo "Good noon user, Have nice day!"
+    echo "This is $day  $dayNo in $month of $year ($timeIn12)"
+elif [ `echo "$timeIn24H >= 14 " | bc ` -eq 1 -a `echo "$timeIn24H < 17 " | bc ` -eq 1 ]
+then
+    echo "Good  afternoon user, Have nice day!"
+    echo "This is $day  $dayNo in $month of $year ($timeIn12)"
+elif [ `echo "$timeIn24H >= 17 " | bc ` -eq 1 -a `echo "$timeIn24H < 21 " | bc ` -eq 1 ]
+then
+    echo "Good evening user, Have nice day!"
+    echo "This is $day  $dayNo in $month of $year ($timeIn12)"
+elif [ `echo "$timeIn24H >= 21 && $timeIn24H<24 " | bc ` -eq 1 -o `echo "$timeIn24H >= 0 && $timeIn24H < 5 " | bc ` -eq 1 ]
+then
+    echo "Good night user, Have nice day!"
+    echo "This is $day  $dayNo in $month of $year ($timeIn12)"
 fi
+
+
+#to get the functionality of this script running when user loggs into shell export bash <this_script_name.sh> into .bashrc in ~
--- assignments/LinuxSys/33_assignment.sh
+++ assignments/LinuxSys/33_assignment.sh
@@ -1,42 +1,78 @@
 <<comments
 name: Ganesh Prasad R
 
-date: 18/10/2021
+date: 9/11/2021
 
-description: 
-Read 'n' and generate a pattern given below
-1
-1 2
-1 2 3
-1 2 3 4
+description: Write a script to replace 20% lines in a C file randomly and replace it with the pattern
 
-input : bash 01_assignment.sh
-Enter the number : 4
+input : ./33_replace<DEL>.sh main.c
 
 output: 
-1
-1 2
-1 2 3
-1 2 3 4
+Before replacing
+#incude <stdio.h>
+int main()
+{
+         printf(“Hello world\n”);
+}
+After replacing
+#incude <stdio.h>
+int main()
+{
+<-----------Deleted------------>
+}
+
 
 comments
 
 #!/bin/bash
 
-read -p "Enter the number :" num
+findNValue () #gives value of N that is 20 % of total lines in a file
+{
+    noOfLines=`wc -l < $1`
+    ans=`echo "  ( 20 * $noOfLines ) / 100 " | bc`
+    echo "$ans"
+}
 
-if [ $num -ge 2 -a $num -le `echo "2 * 2 * 2 * 2 * 2" | bc` ]
-then
-    for row in $(seq 1 $num)
+replceNLine()  #replaces $1 = n lines in given file $2 = file
+{   
+    noOfLines=`wc -l < $2` #no of lines in $2
+    lines=`shuf -i 1-$noOfLines | head -$1`        #get random n line numbers
+    for num in ${lines[@]}
     do
-        displayNumber=1
-        for col in $(seq 1 $row)        
-        do
-            echo -n "$displayNumber "
-            ((displayNumber=displayNumber + 1))
-        done
-        echo
+        `sed -i "${num}s/^.*$/<-----------Deleted------------>/" $2`      #replace the line with given pattern
     done
-else
-    echo "Error : Number out of range, Please enter 2 < number < 2^5"
+}
+
+
+
+
+if [ $# -eq 0 ]
+then
+    echo "Error : Please pass the file name through command line."
+elif [ $# -eq 1 ] #if 1 arg is given
+then
+    if [ -f $1 ] #given arg is a file
+    then 
+        if [ -s $1 ] #file is non empty
+        then
+
+            echo "Before replacing :"
+            echo
+            cat $1 #display content
+            echo
+            n=`findNValue $1` #find n val
+
+            replceNLine $n $1 #replace 20% line
+            echo "After replaced :"
+            echo
+            cat $1
+            echo     
+        else
+            echo "Error : $1 is empty file. So can’t replace the string."
+        fi
+    else
+        echo "Error : No such a file."
+    fi
+else #more than one command line argument is given
+    echo "please send only one argument to the script"
 fi
