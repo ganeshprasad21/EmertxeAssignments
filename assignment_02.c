4ba8106748819beee7e630a3831ec31f2a8547df
theBluePhilosopher <ganesh21111999@gmail.com>
2021-11-30 20:57:27 +0530

    new

--- assignment_02.c
+++ assignment_02.c
@@ -15,38 +15,39 @@ sample output=
 */
 
 #include <stdio.h>
-void fib(int n)
+
+void fib(int n) //func to calculate fibonacci
 {
-    if(n == 0)
-    printf("0\n");
-    else if(n == 1)
-    printf("0,1,1\n");
+    if (n == 0)
+        printf("0\n"); //if its 0
+    else if (n == 1)
+        printf("0,1,1\n"); //if 1
     else
     {
-        int n1=0;
-        int n2=1;
-        int fibn = n1+n2;
-        printf("0,1");
+        int n1 = 0;
+        int n2 = 1;
+        int fibn = n1 + n2;
+        printf("0,1"); //basically its going to be there
         while (n >= fibn)
         {
-            if(fibn <= n)
-                printf(",%d",fibn);
-            n1=n2;
-            n2=fibn;
-            fibn=n1+n2;
+            if (fibn <= n)
+                printf(",%d", fibn);
+            n1 = n2;
+            n2 = fibn;
+            fibn = n1 + n2;
         }
-        printf("\n");
+        printf("\n"); //print empty line
     }
 }
 int main()
 {
     int n;
     printf("Enter a number:");
-    scanf("%d",&n);
+    scanf("%d", &n);
     getchar();
     if (n < 0)
     {
-        printf("please enter n > 0\n")
+        printf("Invalid input\n"); //if negative number is given
     }
     else
     {
